f754d90b40dd7fdd6df405d4998dc46d292d8fbd
Horotan Gelu <horotangelu17@gmail.com>
2022-09-15 17:25:43 +0300

    Added JWT Auth Token, register method

--- models/User.js
+++ models/User.js
@@ -3,24 +3,24 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
-    required: true
-    unique: true
+    required: true,
+    unique: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
   avatar: {
     type: String,
-  }
+  },
   date: {
-    type: Data,
+    type: Date,
     default: Date.now,
-  }
-})
+  },
+});
 
-module.exports = User = mongoose.model('user',UserSchema)
+module.exports = mongoose.model('user', UserSchema);
