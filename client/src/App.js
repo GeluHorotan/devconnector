c18585add295cf137ad955e47333177298246f17
Horotan Gelu <horotangelu17@gmail.com>
2022-09-16 23:21:31 +0300

    Private routing has been added and the login system has been implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
+import { loadUser } from './actions/auth';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -12,36 +15,52 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+import setAuthToken from './utils/setAuthToken';
+
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
+  const location = useLocation();
+  useEffect(() => {
+    store.dispatch(loadUser);
+  }, []);
+
   return (
     <Provider store={store}>
-      <Router>
-        <Navbar />
-        <Alert />
-        <Routes>
-          <Route exact path='/' element={<Landing />} />
+      <Navbar />
+      <Alert />
+      <Routes location={location} key={location.pathname}>
+        <Route exact path='/' element={<Landing />} />
 
-          <Route
-            exact
-            path='/register'
-            element={
-              <section className='container'>
-                <Register />
-              </section>
-            }
-          ></Route>
-          <Route
-            exact
-            path='/login'
-            element={
-              <section className='container'>
-                <Login />
-              </section>
-            }
-          ></Route>
-        </Routes>
-      </Router>
+        <Route
+          exact
+          path='/register'
+          element={
+            <section className='container'>
+              <Register />
+            </section>
+          }
+        />
+        <Route
+          exact
+          path='/login'
+          element={
+            <section className='container'>
+              <Login />
+            </section>
+          }
+        />
+        <Route
+          exact
+          path='/dashboard'
+          element={<PrivateRoute component={<Dashboard />} />}
+        />
+      </Routes>
     </Provider>
   );
 };
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
+
 import { setAlert } from '../../actions/alert';
 import { register } from '../../actions/auth';
-import { v4 as uuidv4 } from 'uuid';
+
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert, register }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -27,6 +28,11 @@ const Register = ({ setAlert, register }) => {
       register({ name, email, password });
     }
   };
+
+  // Redirect if registered
+  if (isAuthenticated) {
+    return <Navigate to='/dashboard' />;
+  }
 
   return (
     <section className='container'>
@@ -87,6 +93,11 @@ const Register = ({ setAlert, register }) => {
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
   register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
 };
 
-export default connect(null, { setAlert, register })(Register);
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
