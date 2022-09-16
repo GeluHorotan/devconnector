55dec4880dbb8acda7ca1b8efdec07d7494e56c0
Horotan Gelu <horotangelu17@gmail.com>
2022-09-16 19:58:21 +0300

    Implementing the register functionality

--- client/src/App.js
+++ client/src/App.js
@@ -11,12 +11,14 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
 
 const App = () => {
   return (
     <Provider store={store}>
       <Router>
         <Navbar />
+        <Alert />
         <Routes>
           <Route exact path='/' element={<Landing />} />
 
